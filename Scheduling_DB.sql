2093851e3cb409135fefdb6618c7f864dde1a89c
zzn2807 <35931343+zzn2807@users.noreply.github.com>
2025-05-15 14:11:33 -0500

    Added schedule database sql file.
    This can be used to generate the database from scratch and seed it with test schedules

---
+++ Scheduling_DB.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS SCHEDULING_DB;
+CREATE DATABASE IF NOT EXISTS SCHEDULING_DB;
+
+CREATE TABLE SCHEDULING_DB.therapists (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    first_name varchar(255),
+    last_name varchar(255),
+    email varchar(255) DEFAULT NULL,
+    phone char(10) DEFAULT NULL
+);
+
+CREATE TABLE SCHEDULING_DB.therapist_schedule (
+	therapist_id int,
+    app_date date,
+    start_time time,
+    end_time time,
+    booked bool DEFAULT 0,
+    client_first_name varchar(255) DEFAULT NULL,
+    client_last_name varchar(255) DEFAULT NULL,
+    FOREIGN KEY (therapist_id) REFERENCES therapists(id)
+);
+
+INSERT INTO SCHEDULING_DB.therapists 
+(first_name, last_name, email, phone)
+VALUES	('Jane','Doe','janedoe@hopehealthsystems.com','5555555555'),
+		('John','Doe', 'johndoe@hopehealthsystems.com','1111111111');
+        
+INSERT INTO SCHEDULING_DB.therapist_schedule 
+(therapist_id, app_date, start_time, end_time)
+VALUES	(1,'2025-05-16','09:00:00','10:00:00'),
+		(1,'2025-05-16','10:00:00','11:00:00'),
+        (1,'2025-05-16','11:00:00','12:00:00'),
+        (1,'2025-05-16','12:00:00','13:00:00'),
+        (1,'2025-05-16','13:00:00','14:00:00'),
+        (1,'2025-05-16','14:00:00','15:00:00'),
+        (1,'2025-05-16','15:00:00','16:00:00'),
+        (1,'2025-05-16','16:00:00','17:00:00'),
+        (2,'2025-05-16','09:00:00','10:00:00'),
+		(2,'2025-05-16','10:00:00','11:00:00'),
+        (2,'2025-05-16','11:00:00','12:00:00'),
+        (2,'2025-05-16','12:00:00','13:00:00'),
+        (2,'2025-05-16','13:00:00','14:00:00'),
+        (2,'2025-05-16','14:00:00','15:00:00'),
+        (2,'2025-05-16','15:00:00','16:00:00'),
+        (2,'2025-05-16','16:00:00','17:00:00');
