a7d3e9929bbe96dc8f5dab862c36320af5b437ee
David Nwokolo <nwokolodz@gmail.com>
2025-05-29 17:00:19 -0400

    Working on scheduling app adding slots

--- NodeJS_Implementation/routes/scheduler.js
+++ NodeJS_Implementation/routes/scheduler.js
@@ -3,9 +3,93 @@ const router = express.Router();
 const scheduling_db = require('../models/scheduling_db');
 const {therapists} = scheduling_db.models;
 const {therapist_schedule} = scheduling_db.models;
-
+const { Op } = require('sequelize');
 //Helper functions
-async function queryBuilder(table, data){
+function timeDifference(start_time,end_time){
+    return (+end_time.split(':')[0] - +start_time.split(':')[0])+ (+end_time.split(':')[1] - +start_time.split(':')[1])/60 ;
+}
+async function scheduleHandler(table, data){
+    let msg = {msg:'Action Completed', msgType:'posMsg'};
+    //Make sure time and date ranges are valid
+    if(timeDifference(data.time_range_start,data.time_range_end)<=0){
+        msg = {msg:'Time range end must be after time range start', msgType:'negMsg'};
+        return msg;
+    }
+    if(new Date(`${data.date_range_end} EST`) - new Date(`${data.date_range_start} EST`)<=0){
+        msg = {msg:'Date range end must be after date range start. The end date is not inclusive', msgType:'negMsg'};
+        return msg;
+    }
+    if(data['user-action'].toLowerCase() === 'add'){
+        data =  (({'user-action': user_action, ...object})=>object)(data);
+        let days = [];
+        let slots = [];
+        let day = new Date(`${data.date_range_start} EST`);
+        while(day.toISOString().split('T')[0]!=data.date_range_end){
+            days.push(day.toISOString().split('T')[0]);
+            day.setDate(day.getDate()+1);
+        }
+        if(timeDifference(data.time_range_start,data.time_range_end)<1){
+            let slot = {
+                therapist_id: data.id,
+                start_time: data.time_range_start, 
+                end_time: data.time_range_end, 
+                booked: +data.booked, 
+                client_first_name: data.client_first_name,
+                client_last_name: data.client_last_name
+            }
+
+            slots.push(slot);
+        }
+        else{
+            let time = data.time_range_start;
+            while(timeDifference(time,data.time_range_end)>=1){
+                let slot = {
+                    therapist_id: data.id,
+                    start_time: `${time}:00`, 
+                    end_time: `${(+time.split(':')[0]+1).toString().padStart(2,'0')}:${time.split(':')[1]}:00`, 
+                    booked: +data.booked, 
+                    client_first_name: data.client_first_name,
+                    client_last_name: data.client_last_name
+                }
+
+                slots.push(slot);
+                time = `${(+time.split(':')[0]+1).toString().padStart(2,'0')}:${time.split(':')[1]}`;
+            }    
+        }
+        console.log(days);
+        console.log(slots);
+        for(let day of days){
+            for(let slot of slots){
+                //Look for overlapping slots
+                let overlap = await table.findAll({
+                    where: {
+                        app_date: day,
+                        [Op.or]: [
+                            {
+                                [Op.and]: [{start_time:{[Op.lte]: slot.start_time}},{end_time:{[Op.gt]: slot.start_time}}]
+                            },
+                            {
+                                [Op.and]: [{start_time:{[Op.lte]: slot.end_time}},{end_time:{[Op.gt]: slot.end_time}}]
+                            }
+                        ]
+                    }
+                });
+                if(overlap.length===0){
+                    slot = {app_date: day, ...slot};
+                    let newSlot = table.build(slot);
+                    console.log(JSON.stringify(newSlot));
+                    // newSlot.save();
+                }
+                else{
+                    msg = {msg:'Some of the slots you created overlap with others that exist so were not added.', msgType:'negMsg'};
+                }
+            }
+        }
+    }
+    return msg;
+}
+
+async function therapistHandler(table, data){
     let msg = {msg:'Action Completed', msgType:'posMsg'};
     if(data['user-action'].toLowerCase() === 'add'){
         let tableData =  (({'user-action': user_action, ...object})=>object)(data);
@@ -74,11 +158,69 @@ router.get('/therapists',(req,res)=>{
 });
 
 router.post('/therapists',(req,res)=>{
-    queryBuilder(therapists,req.body).then((msg)=>{
+    therapistHandler(therapists,req.body).then((msg)=>{
         console.log(msg);
         res.render('therapist',msg);
     });
     
-})
+});
+
+router.get('/schedule',(req,res)=>{
+    therapists.findAll({
+        attributes: ['id', 'first_name', 'last_name']
+    })
+    .then((result)=>{
+        let therapists = JSON.parse(JSON.stringify(result)); 
+        res.render('schedule',{therapists});
+    });
+});
+
+router.post('/schedule',(req,res)=>{
+    res.redirect(`/schedule/${req.body.therapist}`);
+});
+
+router.get('/schedule/:id',(req,res)=>{
+    therapists.findAll({
+        attributes: ['id', 'first_name', 'last_name'],
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((result)=>{
+        let therapist = JSON.parse(JSON.stringify(result));
+        if(therapist.length> 0){
+            res.render('schedule', {therapist: therapist[0]});
+        }
+        else{
+            let msg = {msg:'This therapist does not exist', msgType:'negMsg'};
+            res.render('schedule',msg);
+        }
+    });
+    
+});
+
+router.post('/schedule/:id',(req,res)=>{
+    therapists.findAll({
+        attributes: ['id', 'first_name', 'last_name'],
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((result)=>{
+        let therapist = JSON.parse(JSON.stringify(result));
+        if(therapist.length> 0){
+            scheduleHandler(therapist_schedule,{id: req.params.id, ...req.body})
+            .then((msg)=>{
+                console.log({therapist: therapist[0],...msg});
+                res.render('schedule', {therapist: therapist[0],...msg});
+            });
+            
+        }
+        else{
+            let msg = {msg:'This therapist does not exist', msgType:'negMsg'};
+            res.render('schedule',msg);
+        }
+    });
+});
 
 module.exports = router;
